76cc07e2fd86588537511c4fdd62f7286b998234
JackK1014 <jking1014456@gmail.com>
2024-09-18 15:21:41 -0400

    feat: added check_transfer function to check if a transfer is allowed to go through based on the transfer limit

--- checking_account.py
+++ checking_account.py
@@ -9,3 +9,11 @@ class CheckingAccount(BankAccount):
         self._account_number = account_number  # Protected member
         self.__routing_number = routing_number  # Private member
         self.transfer_limit = transfer_limit
+
+    def check_transfer(self, transfer_amount):
+        if transfer_amount > self.transfer_limit:
+            print("Not allowed to transfer.")
+            return False
+        else:
+            print("Transfer allowed.")
+            return True
