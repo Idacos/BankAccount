86f614937f3bd1d47539a9b2334f8a7997897049
Ilton da Costa Neto <idacosneto@gmail.com>
2024-09-18 15:19:43 -0400

    feat: add_interest method added to savings_account.py

--- savings_account.py
+++ savings_account.py
@@ -3,10 +3,14 @@
 from bank_account import BankAccount
 
 class SavingsAccount(BankAccount):
-    def __init__(self, account_number, routing_number, customer_name, current_balance, minimum_balance):
+    def __init__(self, account_number, routing_number, interest_rate, customer_name, current_balance, minimum_balance):
         super().__init__(customer_name, current_balance, minimum_balance)
         self._account_number = account_number  # Protected member
         self.__routing_number = routing_number  # Private member
+        self.interest_rate = interest_rate
 
     def add_interest(self):
         interest = self.current_balance * self.interest_rate
+        self.current_balance += interest
+        print(f"Interest added: {interest}, New balance: {self.current_balance}")
+
