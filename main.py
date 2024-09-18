92e4020d0e277444fe8aed1f80375d1034424cc6
JackK1014 <jking1014456@gmail.com>
2024-09-18 15:00:09 -0400

    fix: removed unnecessary code from the main.py file

--- main.py
+++ main.py
@@ -1,42 +1,6 @@
 #Author: Ilton da Costa Neto
 #Partner: Jack King
 
-class BankAccount:
-
-    #class attribute
-    title = "The Best Bank in the World"
-
-    #constructor
-    def __init__(self, customer_name, current_balance, minimum_balance):
-        self.customer_name = customer_name
-        self.current_balance = current_balance
-        self.minimum_balance = minimum_balance
-
-    #method 1
-    def deposit(self):
-        amount = int(input("Enter amount to deposit: "))
-        if amount <= self.minimum_balance:
-            print()
-        self.current_balance += amount
-
-    #method 2
-    def withdraw(self):
-        amount = int(input("Enter amount to withdraw: "))
-        calculation = self.current_balance - amount
-        if calculation < self.minimum_balance:
-            print("\nMinimum balance not met, cannot withdraw\n")
-        else:
-            self.current_balance -= amount
-            print(f"Your current balance is now {self.current_balance}")
-
-    #method 3
-    def print_customer_information(self):
-        print("\n")
-        print(BankAccount.title)
-        print(f"Customer Name: {self.customer_name}")
-        print(f"Current Balance: {self.current_balance}")
-        print(f"Minimum Balance: {self.minimum_balance} \n")
-
 class Savings(BankAccount):
     def __init__(self, account_number, routing_number, customer_name, current_balance, minimum_balance):
         super().__init__(customer_name, current_balance, minimum_balance)
