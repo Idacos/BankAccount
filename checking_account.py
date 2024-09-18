33ec49c89ecb19cee8aecdd38505854b35f28e66
JackK1014 <jking1014456@gmail.com>
2024-09-18 15:03:32 -0400

    feat: moved the correct code over to each file and imported bank account

--- checking_account.py
+++ checking_account.py
@@ -1,2 +1,10 @@
 #Author: Ilton da Costa Neto
 #Partner: Jack King
+
+from bank_account import BankAccount
+
+class Checking(BankAccount):
+    def __init__(self, account_number, routing_number, customer_name, current_balance, minimum_balance):
+        super().__init__(customer_name, current_balance, minimum_balance)
+        self._account_number = account_number  # Protected member
+        self.__routing_number = routing_number  # Private member
