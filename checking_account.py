8ff4f83ed8a335f860a7c34cba7cc89fea1b7f39
JackK1014 <jking1014456@gmail.com>
2024-09-18 15:13:08 -0400

    feat: added a transfer_limitation attribute

--- checking_account.py
+++ checking_account.py
@@ -3,8 +3,9 @@
 
 from bank_account import BankAccount
 
-class Checking(BankAccount):
-    def __init__(self, account_number, routing_number, customer_name, current_balance, minimum_balance):
+class CheckingAccount(BankAccount):
+    def __init__(self, account_number, routing_number, customer_name, current_balance, minimum_balance, transfer_limit):
         super().__init__(customer_name, current_balance, minimum_balance)
         self._account_number = account_number  # Protected member
         self.__routing_number = routing_number  # Private member
+        self.transfer_limit = transfer_limit
